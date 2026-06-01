16618279f09518f4974c1ac45658866b28e20004
Yan Zinchenko <12407564+YanZinchenko@users.noreply.github.com>
2026-06-01 17:16:36 +0200

    init multi-module project

---
+++ customer/src/main/java/com/zim4ik/App.java
@@ -0,0 +1,13 @@
+package com.zim4ik;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
